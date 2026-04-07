b2cb927a42d0c5419aca6257fa431203231fc9b9
jank-hub <rojasjank953@gmail.com>
2026-04-07 17:56:44 -0500

    Add files via upload

---
+++ juego.js
@@ -0,0 +1,22 @@
+let numeroSecreto = Math.floor(Math.random() * 5) + 1
+
+function jugar() {
+
+  let intento = Number(document.getElementById("Número").value);
+
+  if (intento === numeroSecreto) {
+
+    document.getElementById("resultado").textContent = 
+    "🎉 ¡Adivinaste el número!";
+
+  } else if (intento < numeroSecreto) {
+
+    document.getElementById("resultado").textContent =
+    "📉 El número es mayor";
+
+  } else {
+
+    document.getElementById("resultado").textContent =
+    "📈 El número es menor"
+  }
+}
